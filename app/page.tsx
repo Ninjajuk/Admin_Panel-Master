983961ffe4e27dcac6027d6a5f04ec068b05fe1f
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-09-18 00:19:03 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className=" font-[family-name:var(--font-geist-sans)]">
+      <h1 className="font-bold text-2xl">Hi Samsu Welcome to the Admin Panel</h1>
+    </div>
+  );
+}
