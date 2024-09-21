37cce40f45f02e5009092f3e83c60ee25c662494
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-09-21 23:43:42 +0530

    21 sep

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className=" font-[family-name:var(--font-geist-sans)]">
-      <h1 className="font-bold text-2xl">Hi Samsu Welcome to the Admin Panel</h1>
+    <div className=" font-[family-name:var(--font-geist-sans)] mx-auto">
+      <h1 className="font-bold text-2xl text-center text-primary py-48">Hi Samsu Welcome to the Admin Panel</h1>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,35 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // Button Colors
+        buttonPrimary: "#3498db",
+        buttonSecondary: "#2ecc71",
+        buttonDanger: "#e74c3c",
+        buttonWarning: "#f39c12",
+
+        primary: "#800000",
+
+        //background
+        bgPrimary: "#f8f9fa",
+        bgSecondary: "#00BFA5" /*Teal Modern and Sleek */,
+        tertary: "#00ACC1",
+        quadliterily: "#8E24AA",
+
+        // Theme Background Colors
+        themeLight: "#ffffff",
+        themeDark: "#000000",
+        themePrimary: "#f8f9fa",
+        themeSecondary: "#ecf0f1",
+
+        // Text Paragraph Colors
+        textPrimary: "#2c3e50",
+        textSecondary: "#95a5a6",
+        textMuted: "#7f8c8d",
+
+        //text Heading
+        txtHeading1: "#008080" /* Teal   Provides a fresh, modern feel.*/,
+        txtHeading2: "#708090" /* Slate Gray */,
+        txtHeading3: "#DC143C" /*Crimson Red: Bold and attention-grabbing.*/,
       },
     },
   },
